f9aa1d9a5976483c1740b51c716a6e21c7da8d40
killerwhalee <killerwhalee@kaist.ac.kr>
2024-10-06 15:19:25 +0000

    Updated root directory

--- samples/solar_panel/solar_panel.py
+++ samples/solar_panel/solar_panel.py
@@ -30,7 +30,7 @@ import numpy as np
 import skimage.draw
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
